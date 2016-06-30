f2c57a650e79130784bea0480c4612e3539b20e0
Tayrone Cordeiro de Menezes Marques <tayronecordeiro@hotmail.com>
2016-06-30 14:09:46 -0300

    tela motivo
    Adicionei a tela motivo

--- App_RumaAe/app/src/main/java/com/example/tayronecordeiro/apprumaae/MainActivity.java
+++ App_RumaAe/app/src/main/java/com/example/tayronecordeiro/apprumaae/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.tayronecordeiro.apprumaae;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent  intent = new Intent(MainActivity.this, Motivo.class);
+                startActivity(intent);
             }
         });
     }
