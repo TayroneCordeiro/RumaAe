2d0e4d80571bd211cfde2cfd60468c051aca56ee
Tayrone Cordeiro de Menezes Marques <tayronecordeiro@hotmail.com>
2016-06-30 14:10:04 -0300

    tela motivo

---
+++ App_RumaAe/app/src/main/java/com/example/tayronecordeiro/apprumaae/Motivo.java
@@ -0,0 +1,13 @@
+package com.example.tayronecordeiro.apprumaae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Motivo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motivo);
+    }
+}
